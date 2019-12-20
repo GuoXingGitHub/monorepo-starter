abd8714e29dcf900d800f7876d8d744719f4cad3
Phassarun Iamampai <phassarun@gmail.com>
2019-12-20 11:53:25 +0700

    [ADD] .env.example.sh

---
+++ .env.example.sh
@@ -0,0 +1,24 @@
+# config/app.ts
+export NODE_ENV=development
+export APP_ENV=local
+export PORT=8080
+
+# config/database.ts
+export TYPEORM_HOST=127.0.0.1
+export TYPEORM_USERNAME=user
+export TYPEORM_PASSWORD=password
+export TYPEORM_DATABASE=database_name
+export TYPEORM_PORT=port
+export TYPEORM_LOGGING=true
+export TYPEORM_MIGRATIONS_TABLE_NAME=migrations
+export TYPEORM_MIGRATIONS_RUN=true
+export TYPEORM_SYNCHRONIZE=true
+
+# config/attachment.ts
+export ATTACHMENT_BUCKET=
+export ATTACHMENT_ORIGIN=
+
+# config/firebase.ts
+export FIREBASE_DATABASE_URL=
+
+# config/email.ts
